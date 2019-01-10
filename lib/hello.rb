c0ea721487ab1d82171d7b70932653522d11c17b
Brandon Brasson <rocksong622@gmail.com>
2019-01-10 23:37:27 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -12,6 +12,6 @@ end
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
-    puts "fails gracefully when a block is not passed in"
+    puts "Hey! No block was given!"
   end
 end
